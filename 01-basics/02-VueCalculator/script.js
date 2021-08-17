9b0167c6570834c2ddceb412d81850a4016afd5e
Айвазян Саргис <smajvaz1@mts.ru>
2021-08-17 16:58:27 +0400

    Solve VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,37 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const Root = defineComponent({
+  name: 'Root',
+
+  data() {
+    return {
+      a: 0,
+      b: 0,
+      picked: '',
+    };
+  },
+
+  computed: {
+    result: function () {
+      switch (this.picked) {
+        case 'sum':
+          return Number(this.a) + Number(this.b);
+        case 'subtract':
+          return Number(this.a) - Number(this.b);
+        case 'multiply':
+          return Number(this.a) * Number(this.b);
+        case 'divide':
+          return Number(this.a) / Number(this.b);
+        default:
+          return 0;
+      }
+    },
+  },
+});
+
+const app = createApp(Root);
+
+const vm = app.mount('#app');
+
+window.vm = vm;
