71ce55bc30c5a233ce71e3d9cd7ae8fdf29ff6ae
Айвазян Саргис <smajvaz1@mts.ru>
2021-08-17 14:32:28 +0400

    Solve CounterButtonUp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,26 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const Root = defineComponent({
+  name: 'Root',
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    ss: function (event) {
+      this.count++;
+    },
+  },
+
+  //template: '',
+});
+
+const app = createApp(Root);
+
+const vm = app.mount('#app');
+
+window.vm = vm;
